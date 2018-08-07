3b8c76980ea6f3fbbfce9a0c913e7f39e0937fb2
Mike Garcia <mg-3@users.noreply.github.com>
2018-08-07 11:25:24 -0600

    Update path

--- irods_start.sh
+++ irods_start.sh
@@ -1,4 +1,4 @@
-#! /usr/bin/env bash
+#!/usr/bin/env bash
 
 INDOCKER_DIR_1="/renci/irods/gtex/Annotations"
 INDOCKER_DIR="/home/dockeruser/venv"
@@ -25,4 +25,4 @@ docker exec -ti dc_jupyter sudo  /home/dockeruser/venv/bin/pip3 install scipy ma
 # Copy into container missing GTEx content and notebook
 docker cp /home/data/GTEx/data/biomart_ENSG2NCBI.tsv dc_jupyter:"$INDOCKER_DIR/gtex"
 docker cp /home/data/GTEx/data/GTEx_Analysis_2016-01-15_v7_RNASeQCv1.1.8_gene_tpm.gct.gz dc_jupyter:"$INDOCKER_DIR/gtex"
-docker cp ~/jupyter-nb/gtex_rnaseq_prep.ipynb dc_jupyter:"$INDOCKER_DIR/notebooks"
+docker cp gtex_rnaseq_prep.ipynb dc_jupyter:"$INDOCKER_DIR/notebooks"
