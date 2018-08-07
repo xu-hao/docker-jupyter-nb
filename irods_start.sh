3ef9cb56d6e8df236b23466a8e20a4fa0eab1dc1
Mike Garcia <mg-3@users.noreply.github.com>
2018-08-06 22:12:56 -0600

    Irods configuration script

--- irods_start.sh
+++ irods_start.sh
@@ -3,12 +3,20 @@
 INDOCKER_DIR_1="/renci/irods/gtex/Annotations"
 INDOCKER_DIR="/home/dockeruser/venv"
 
+# The paths where the data files reside
 export GTEX_DATA_PATH_1="${INDOCKER_DIR_1}/"
 export GTEX_DATA_PATH_2="${INDOCKER_DIR}/gtex/"
-export GTEX_NB_PATH="${INDOCKER_DIR}/notebooks/"
 
-./helium run jupyter -venv -U mgarcia5@globusid.org -P TempPassword
+if [ -z "$1" ]
+then
+    echo "Usage: ./irods_start.sh <password>"
+    exit 9
+else
+    # The script to start the irods container
+    ./helium run jupyter -venv -U mgarcia5@globusid.org -P $1
+fi
 
+# Add additional setup to Irods container in order to run UNM notebook
 docker exec -ti dc_jupyter mkdir "${INDOCKER_DIR}/notebooks"
 docker exec -ti dc_jupyter mkdir "${INDOCKER_DIR}/gtex"
 docker exec -ti dc_jupyter sudo /home/dockeruser/venv/bin/pip3 install --upgrade pip
